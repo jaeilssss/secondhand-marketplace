c38ae61f813c6890ab11fc730ebb607251357dfa
jaeilssss <jaeilssss@bitbucket.org>
2024-09-01 15:55:10 +0900

    init

---
+++ src/main/java/com/market/secondhandmarketplace/SecondhandMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.market.secondhandmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondhandMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market/secondhandmarketplace/SecondhandMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.secondhandmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
